5f1a5608719dfbf1d82ec5bcfeee7cb458c38b05
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2025-06-14 21:47:27 +0900

    feat: initialize p5js template with responsive canvas example
    - Added package.json with development dependencies for stylelint.
    - Created example.js sketch demonstrating responsive canvas setup and drawing.

---
+++ sketches/example.js
@@ -0,0 +1,35 @@
+const { Responsive } = P5Template;
+
+function setup() {
+  new Responsive().createResponsiveCanvas(400, 300, 'contain', false);
+  // "contain" | "fill" | "cover" | "none" | "scale-down"
+
+  // "contain", true
+  // 주어진 공간에 잘리지 않고 들어옴; 비례 유지; 이미지 확축
+  // data-full-width="false", 너비 1440 추천
+
+  // "contain", false
+  // 주어진 공간에 잘리지 않고 들어옴; 비례 유지; 너비 높이 변화
+  // data-full-width="true" 추천
+
+  // "fill", false
+  // 주어진 공간을 채움; 비례 무시; 너비 높이 변화
+  // data-full-width="true" 추천
+
+  // "scale-down", true
+  // 주어진 공간에 잘리지 않고 들어옴; 비례 유지; 이미지 축소
+  // data-full-width="false", 너비 1440 추천
+}
+
+function draw() {
+  background('#000000');
+  noStroke();
+  fill('red');
+  circle(mouseX, mouseY, 100);
+  Responsive.drawReferenceGrid('white');
+  noStroke();
+  fill('white');
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(`${width}, ${height}`, width / 2, height / 2);
+}
